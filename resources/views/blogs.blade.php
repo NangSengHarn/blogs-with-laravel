d02122294dc9916f186736334e7c06feffe0583b
NangSengHarn <nangsengharn@gmail.com>
2022-03-03 15:17:23 +0630

    setup and drawback of static or hardcoding blogs website

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+
+    <h1><a href="/blog">First Blog</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptatum sunt provident iusto minima voluptatibus. Ipsa minima excepturi nihil tempora molestiae ducimus eveniet consequuntur dolorem sed nobis officiis minus repellendus dolor natus hic assumenda quod molestias illo accusantium, ad doloribus est quae? Nulla aperiam ipsam architecto amet rerum illo ea deleniti, doloremque dolor fuga accusamus voluptates id, repellendus nihil nostrum tempora labore illum expedita laborum fugiat? Ipsam hic nam assumenda aperiam possimus cumque temporibus, quis facilis sit dolor debitis nostrum earum, fugiat iste quisquam sunt minima? Necessitatibus corporis itaque et nihil. Impedit soluta eius quaerat, minus quae provident quod tempora!</p>
+    
+    </article>
+    <article>
+
+    <h1><a href="/blog">Second Blog</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptatum sunt provident iusto minima voluptatibus. Ipsa minima excepturi nihil tempora molestiae ducimus eveniet consequuntur dolorem sed nobis officiis minus repellendus dolor natus hic assumenda quod molestias illo accusantium, ad doloribus est quae? Nulla aperiam ipsam architecto amet rerum illo ea deleniti, doloremque dolor fuga accusamus voluptates id, repellendus nihil nostrum tempora labore illum expedita laborum fugiat? Ipsam hic nam assumenda aperiam possimus cumque temporibus, quis facilis sit dolor debitis nostrum earum, fugiat iste quisquam sunt minima? Necessitatibus corporis itaque et nihil. Impedit soluta eius quaerat, minus quae provident quod tempora!</p>
+    
+    </article>
+</body>
+</html>
