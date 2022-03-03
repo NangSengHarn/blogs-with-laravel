96da62675b5d27fba6bfe6f1803473efaf2c0aec
NangSengHarn <nangsengharn@gmail.com>
2022-03-03 15:41:14 +0630

    pass data to view and wild card

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -8,12 +8,7 @@
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
-    <article>
-
-    <h1><a href="/blog">First Blog</a></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptatum sunt provident iusto minima voluptatibus. Ipsa minima excepturi nihil tempora molestiae ducimus eveniet consequuntur dolorem sed nobis officiis minus repellendus dolor natus hic assumenda quod molestias illo accusantium, ad doloribus est quae? Nulla aperiam ipsam architecto amet rerum illo ea deleniti, doloremque dolor fuga accusamus voluptates id, repellendus nihil nostrum tempora labore illum expedita laborum fugiat? Ipsam hic nam assumenda aperiam possimus cumque temporibus, quis facilis sit dolor debitis nostrum earum, fugiat iste quisquam sunt minima? Necessitatibus corporis itaque et nihil. Impedit soluta eius quaerat, minus quae provident quod tempora!</p>
-    
-    </article>
+    <?= $blog; ?>
     <a href="/">go back</a>
 </body>
 </html>
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -10,15 +10,21 @@
 <body>
     <article>
 
-    <h1><a href="/blog">First Blog</a></h1>
+    <h1><a href="/blogs/first-blog">First Blog</a></h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptatum sunt provident iusto minima voluptatibus. Ipsa minima excepturi nihil tempora molestiae ducimus eveniet consequuntur dolorem sed nobis officiis minus repellendus dolor natus hic assumenda quod molestias illo accusantium, ad doloribus est quae? Nulla aperiam ipsam architecto amet rerum illo ea deleniti, doloremque dolor fuga accusamus voluptates id, repellendus nihil nostrum tempora labore illum expedita laborum fugiat? Ipsam hic nam assumenda aperiam possimus cumque temporibus, quis facilis sit dolor debitis nostrum earum, fugiat iste quisquam sunt minima? Necessitatibus corporis itaque et nihil. Impedit soluta eius quaerat, minus quae provident quod tempora!</p>
     
     </article>
     <article>
 
-    <h1><a href="/blog">Second Blog</a></h1>
+    <h1><a href="/blogs/second-blog">Second Blog</a></h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptatum sunt provident iusto minima voluptatibus. Ipsa minima excepturi nihil tempora molestiae ducimus eveniet consequuntur dolorem sed nobis officiis minus repellendus dolor natus hic assumenda quod molestias illo accusantium, ad doloribus est quae? Nulla aperiam ipsam architecto amet rerum illo ea deleniti, doloremque dolor fuga accusamus voluptates id, repellendus nihil nostrum tempora labore illum expedita laborum fugiat? Ipsam hic nam assumenda aperiam possimus cumque temporibus, quis facilis sit dolor debitis nostrum earum, fugiat iste quisquam sunt minima? Necessitatibus corporis itaque et nihil. Impedit soluta eius quaerat, minus quae provident quod tempora!</p>
     
+    </article>
+    <article>
+
+    <h1><a href="/blogs/third-blog">Third Blog</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptatum sunt provident iusto minima voluptatibus. Ipsa minima excepturi nihil tempora molestiae ducimus eveniet consequuntur dolorem sed nobis officiis minus repellendus dolor natus hic assumenda quod molestias illo accusantium, ad doloribus est quae? Nulla aperiam ipsam architecto amet rerum illo ea deleniti, doloremque dolor fuga accusamus voluptates id, repellendus nihil nostrum tempora labore illum expedita laborum fugiat? Ipsam hic nam assumenda aperiam possimus cumque temporibus, quis facilis sit dolor debitis nostrum earum, fugiat iste quisquam sunt minima? Necessitatibus corporis itaque et nihil. Impedit soluta eius quaerat, minus quae provident quod tempora!</p>
+
     </article>
 </body>
 </html>
