816ed58315dffc9c0d72a5d2041f778d91ef156f
NangSengHarn <nangsengharn@gmail.com>
2022-03-09 15:38:24 +0630

    Collection part-2 _ Sorting

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -11,6 +11,7 @@
     <?php foreach($blogs as $blog): ?>
         <h1><a href="blogs/<?= $blog->slug; ?>"><?= $blog->title; ?></a></h1>
         <div>
+            <p>published at - <?= $blog->date; ?></p>
             <p><?= $blog->intro; ?></p>
         </div>
     <?php endforeach; ?>
