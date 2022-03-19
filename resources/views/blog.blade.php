6e6ab84a46d0c8130833f438c0c620b63f1b9db8
NangSengHarn <nangsengharn@gmail.com>
2022-03-19 19:56:04 +0630

    Blade layout (section and yield)

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-    <h1><?= $blog->title; ?></h1>
-    <p><?= $blog->body; ?></p>
+@extends('layout')
+
+@section('title')
+<title>{{$blog->title}}</title>
+@endsection
+
+@section('content')
+    <h1>{{$blog->title}}</h1>
+    <p>{{$blog->body}}</p>
     <a href="/">go back</a>
-</body>
-</html>
+@endsection
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,19 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-    <?php foreach($blogs as $blog): ?>
-        <h1><a href="blogs/<?= $blog->slug; ?>"><?= $blog->title; ?></a></h1>
-        <div>
-            <p>published at - <?= $blog->date; ?></p>
-            <p><?= $blog->intro; ?></p>
-        </div>
-    <?php endforeach; ?>
-</body>
-</html>
+@extends('layout')
+
+@section('title')
+<title>All Blogs</title>
+@endsection
+
+@section('content')
+@foreach($blogs as $blog)
+  <div>
+      <h1><a href="blogs/{{$blog->slug}}">{{$blog->title}}</a></h1>
+      <div>
+          <p>published at - {{$blog->date}}</p>
+          <p>{{$blog->intro}}</p>
+      </div>
+  </div>  
+@endforeach
+@endsection
+
