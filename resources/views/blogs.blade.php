a32d4ea95cb04549e22189c2e66001b302be96aa
NangSengHarn <nangsengharn@gmail.com>
2022-03-20 20:40:27 +0630

    Blade Layout (component system)

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,18 +1,15 @@
-@extends('layout')
-
-@section('title')
-<title>All Blogs</title>
-@endsection
-
-@section('content')
-@foreach($blogs as $blog)
-  <div>
-      <h1><a href="blogs/{{$blog->slug}}">{{$blog->title}}</a></h1>
-      <div>
-          <p>published at - {{$blog->date}}</p>
-          <p>{{$blog->intro}}</p>
-      </div>
-  </div>  
-@endforeach
-@endsection
+<x-layout>
+  <x-slot>
+    <title>All Blogs</title>
+  </x-slot>
+  @foreach($blogs as $blog)
+    <div>
+        <h1><a href="blogs/{{$blog->slug}}">{{$blog->title}}</a></h1>
+        <div>
+            <p>published at - {{$blog->date}}</p>
+            <p>{{$blog->intro}}</p>
+        </div>
+    </div>  
+  @endforeach
+</x-layout>
 
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @yield('title')
+    {{$title}}
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
-    @yield('content')
+    <x-navbar />
+    {{$slot}}
 </body>
 </html>
