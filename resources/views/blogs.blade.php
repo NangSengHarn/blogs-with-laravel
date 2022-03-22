6d278c92549bbbc92be7973bc0d0cfb34afd2f5f
NangSengHarn <nangsengharn@gmail.com>
2022-03-22 10:30:38 +0630

    Blog Model and Migration with Database

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -4,7 +4,7 @@
   </x-slot>
     @foreach($blogs as $blog)
       <div>
-          <h1><a href="blogs/{{$blog->slug}}">{{$blog->title}}</a></h1>
+          <h1><a href="blogs/{{$blog->id}}">{{$blog->title}}</a></h1>
           <div>
               <p>published at - {{$blog->date}}</p>
               <p>{{$blog->intro}}</p>
