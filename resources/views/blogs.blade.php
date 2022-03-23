29f8f55ea87ba7805fb83eb3e1df5aefb966b26b
NangSengHarn <nangsengharn@gmail.com>
2022-03-23 19:19:49 +0630

    Find a blog by it's slug

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -4,7 +4,7 @@
   </x-slot>
     @foreach($blogs as $blog)
       <div>
-          <h1><a href="blogs/{{$blog->id}}">{{$blog->title}}</a></h1>
+          <h1><a href="blogs/{{$blog->slug}}">{{$blog->title}}</a></h1>
           <div>
               <p>published at - {{$blog->created_at->diffForHumans()}}</p>
               <p>{{$blog->intro}}</p>
