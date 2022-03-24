b57f7a5152b883364fdc89192140458101ad5706
NangSengHarn <nangsengharn@gmail.com>
2022-03-24 13:18:36 +0630

    use factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,28 +21,10 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create();
 
-        $frontend=Category::create([
-            'name'=>'frontend',
-            'slug'=>'frontend'
-        ]);
-        $backend=Category::create([
-            'name'=>'backend',
-            'slug'=>'backend'
-        ]);
+        $frontend=Category::factory()->create(['name'=>'frontend']);
+        $backend=Category::factory()->create(['name'=>'backend']);
 
-        Blog::create([
-            'title'=>'Frontend Post',
-            'slug'=>'frontend-blog',
-            'intro'=>'This is intro',
-            'body'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'category_id'=>$frontend->id
-        ]);
-        Blog::create([
-            'title'=>'backend Post',
-            'slug'=>'backend-blog',
-            'intro'=>'This is intro',
-            'body'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'category_id'=>$backend->id
-        ]);
+        Blog::factory(2)->create(['category_id'=>$frontend->id]);
+        Blog::factory(3)->create(['category_id'=>$backend->id]);
     }
 }
