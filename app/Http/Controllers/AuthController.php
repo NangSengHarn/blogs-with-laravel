b050eaa29ae092725bd0c565445dd42a4a17268f
NangSengHarn <nangsengharn@gmail.com>
2022-03-31 23:52:40 +0630

    store data to database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -10,12 +12,14 @@ class AuthController extends Controller
         return view('register.create');
     }
     public function store(){
-        request()->validate([
-            'name'=>'required',
-            'email'=>'required|email',
-            'username'=>'required',
+        $formData=request()->validate([
+            'name'=>'required|min:3',
+            'email'=>['required','email',Rule::unique('users','email')],
+            'username'=>['required','min:3',Rule::unique('users','username')],
             'password'=>'required|min:8'
         ]);
 
+        User::create($formData);
+        return redirect('/');
     }
 }
