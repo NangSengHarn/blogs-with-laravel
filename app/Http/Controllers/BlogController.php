bcf4996bbe698d00d98ea98bd5146c6f4f231168
NangSengHarn <nangsengharn@gmail.com>
2022-03-31 20:54:19 +0630

    registration design integration

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -11,12 +11,12 @@ class BlogController extends Controller
 {
     public function index(){
         
-        return view('blogs',[
+        return view('blogs.index',[
             'blogs'=>Blog::latest()->filter(request(['search','category','username']))->paginate(6)->withQueryString()
         ]);
     }
     public function show(Blog $blog){
-        return view('blog',[
+        return view('blogs.show',[
             'blog'=>$blog,
             'randomBlogs'=>Blog::inRandomOrder()->take(3)->get()
         ]);
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\AuthController;
 use App\Models\Blog;
 use App\Models\Category;
 use App\Models\User;
@@ -12,3 +13,4 @@ Route::get('/', [BlogController::class,'index']);
 Route::get('/blogs/{blog:slug}', [BlogController::class,'show'])->where('blog','[A-z\d\-_]+');
 
 
+Route::get('/register',[AuthController::class,'create']);
