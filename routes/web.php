c8cd15f61e4ebc70a90907c3cf07d2da3ca51d89
NangSengHarn <nangsengharn@gmail.com>
2022-04-03 11:52:29 +0630

    use middleware

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,12 @@ Route::get('/', [BlogController::class,'index']);
 
 Route::get('/blogs/{blog:slug}', [BlogController::class,'show'])->where('blog','[A-z\d\-_]+');
 
-Route::get('/register',[AuthController::class,'create']);
+Route::get('/register',[AuthController::class,'create'])->middleware('guest');
 
-Route::post('/register',[AuthController::class,'store']);
+Route::post('/register',[AuthController::class,'store'])->middleware('guest');
 
-Route::post('/logout',[AuthController::class,'logout']);
+Route::post('/logout',[AuthController::class,'logout'])->middleware('auth');
 
-Route::get('/login',[AuthController::class,'login']);
+Route::get('/login',[AuthController::class,'login'])->middleware('guest');
 
-Route::post('/login',[AuthController::class,'post_login']);
+Route::post('/login',[AuthController::class,'post_login'])->middleware('guest');
